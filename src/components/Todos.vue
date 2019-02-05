d546570728146180e0c2a9ee984d48c73ffe6a3e
Ricardo Mayo <rmayo.1995@gmail.com>
2019-02-04 19:19:54 -0600

    First commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>TO DOS</h1>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <!-- <h3 >{{todo.title}}</h3> -->
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from '../components/TodoItem'
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>This is the ToDO List App v. 1.0.0 :)</p>
+  </div>
+</template>
